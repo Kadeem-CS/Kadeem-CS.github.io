d1429462880ae7bf9f7fd46256790379ef13a57d
Kadeem-CS <kadeemcollins10@gmail.com>
2024-10-31 15:58:34 -0400

    Update script.js

--- my-3d-game/script.js
+++ my-3d-game/script.js
@@ -1,1 +1,25 @@
+const canvas = document.getElementById("renderCanvas");
+const engine = new BABYLON.Engine(canvas, true);
+
+const createScene = () => {
+    const scene = new BABYLON.Scene(engine);
+    const camera = new BABYLON.ArcRotateCamera("camera", Math.PI / 2, Math.PI / 2.5, 10, BABYLON.Vector3.Zero(), scene);
+    camera.attachControl(canvas, true);
+    new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+    const ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 10, height: 10 }, scene);
+    const box = BABYLON.MeshBuilder.CreateBox("box", {}, scene);
+    box.position.y = 1;
+
+    return scene;
+};
+
+const scene = createScene();
+
+engine.runRenderLoop(() => {
+    scene.render();
+});
+
+window.addEventListener("resize", () => {
+    engine.resize();
+});
 
