ab997e38d1c329c710931b2c5cd36489f77f8545
Kadeem-CS <kadeemcollins10@gmail.com>
2026-04-02 12:39:18 -0400

    Create App.jsx

---
+++ react/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ChessGame from "./ChessGame";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>♟ React Chess Enhanced</h1>
+      <ChessGame />
+    </div>
+  );
+}
